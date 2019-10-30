762b9fd4b3abc6c71fa28bf65c8ea8845d9f6e81
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-10-30 14:08:41 +0400

    ajouter forme de superviseur

---
+++ Infomil/Superviseur.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Infomil
+{
+    partial class Superviseur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgDetaileClient = new System.Windows.Forms.DataGridView();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnVisualiser = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.btnSeDeconnecter = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDetaileClient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(775, 47);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Information";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Superviseur - Acces aux donnees de tous les rayons et clients du magasin";
+            // 
+            // dgDetaileClient
+            // 
+            this.dgDetaileClient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDetaileClient.Location = new System.Drawing.Point(13, 67);
+            this.dgDetaileClient.Name = "dgDetaileClient";
+            this.dgDetaileClient.Size = new System.Drawing.Size(675, 363);
+            this.dgDetaileClient.TabIndex = 1;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(694, 67);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(93, 23);
+            this.btnAjouter.TabIndex = 2;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // btnVisualiser
+            // 
+            this.btnVisualiser.Location = new System.Drawing.Point(694, 97);
+            this.btnVisualiser.Name = "btnVisualiser";
+            this.btnVisualiser.Size = new System.Drawing.Size(93, 23);
+            this.btnVisualiser.TabIndex = 3;
+            this.btnVisualiser.Text = "Visualiser";
+            this.btnVisualiser.UseVisualStyleBackColor = true;
+            this.btnVisualiser.Click += new System.EventHandler(this.btnVisualiser_Click);
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(694, 127);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(93, 23);
+            this.btnSupprimer.TabIndex = 4;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(694, 406);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(93, 23);
+            this.btnQuitter.TabIndex = 5;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // btnSeDeconnecter
+            // 
+            this.btnSeDeconnecter.Location = new System.Drawing.Point(694, 377);
+            this.btnSeDeconnecter.Name = "btnSeDeconnecter";
+            this.btnSeDeconnecter.Size = new System.Drawing.Size(93, 23);
+            this.btnSeDeconnecter.TabIndex = 6;
+            this.btnSeDeconnecter.Text = "Se deconnecter";
+            this.btnSeDeconnecter.UseVisualStyleBackColor = true;
+            this.btnSeDeconnecter.Click += new System.EventHandler(this.btnSeDeconnecter_Click);
+            // 
+            // Superviseur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSeDeconnecter);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnVisualiser);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.dgDetaileClient);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Superviseur";
+            this.Text = "Superviseur";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDetaileClient)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgDetaileClient;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnVisualiser;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.Button btnSeDeconnecter;
+    }
+}
---
+++ Infomil/Superviseur.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Infomil
+{
+    public partial class Superviseur : Form
+    {
+        public Superviseur()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVisualiser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSeDeconnecter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
