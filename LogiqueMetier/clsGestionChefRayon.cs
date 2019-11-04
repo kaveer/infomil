921ed33343fc1da621a9bc6b4700205ec96ac53e
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-11-04 20:18:44 +0400

    implement class n inheritance

---
+++ LogiqueMetier/clsGestionChefRayon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogiqueMetier
+{
+    class clsGestionChefRayon : clsGestionPersonnes
+    {
+        public override void CreerPersonne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RecupererListePersonnes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SupprimerPersonne()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LogiqueMetier/clsGestionPersonnes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogiqueMetier
+{
+    public abstract class clsGestionPersonnes
+    {
+        public abstract void CreerPersonne();
+        public abstract void RecupererListePersonnes();
+        public abstract void SupprimerPersonne();
+
+        public void AuthentifierPersonne()
+        {
+
+        }
+
+        public void ModifierPersonnes()
+        {
+
+        }
+
+        public void RecupererPanierPersonne()
+        {
+
+        }
+
+    }
+}
